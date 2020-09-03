a8b4bb65e0aa48ad6d2a7571cde21f7d8fdea061
Stanislau Klimovich <stasklimovich299@gmail.com>
2020-09-03 23:32:49 +0300

    base classes of vehicles was added #3

---
+++ 1.6/vehicles/base.py
@@ -0,0 +1,44 @@
+
+from abc import ABCMeta, abstractmethod
+
+
+class BaseVehicle:
+    """
+    Base vehicle class
+    """
+
+    WEIGHT = 0
+    PAYLOAD = 0
+    FUEL_CONSUMPTION = 0
+    SOUND = '...'
+
+    def make_sound(self):
+        print(self.SOUND)
+
+
+class BaseCar(BaseVehicle, metaclass=ABCMeta):
+
+    def __init__(self, vendor: str, model: str):
+        self.vendor = vendor
+        self.model = model
+
+    @abstractmethod
+    def ride(self, distance):
+        # raise NotImplementedError
+        pass
+
+
+class BaseShip(BaseVehicle, metaclass=ABCMeta):
+
+    def __init__(self, vendor: str, model: str, name: str):
+        self.vendor = vendor
+        self.model = model
+        self.name = name
+
+    @abstractmethod
+    def sail(self, distance):
+        # raise NotImplementedError
+        pass
+
+
+
