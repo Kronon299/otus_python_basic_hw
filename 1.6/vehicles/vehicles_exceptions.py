0e4a7062633c339ccc9a794a70a708f27852cb5d
Stanislau Klimovich <stasklimovich299@gmail.com>
2020-09-09 00:37:35 +0300

    LowFuelError exception was improved #3

--- 1.6/vehicles/vehicles_exceptions.py
+++ 1.6/vehicles/vehicles_exceptions.py
@@ -8,5 +8,10 @@ class PositiveValueError(ValueError):
 
 
 class LowFuelError(ValueError):
-    pass
+    def __init__(self, present_value, need_value):
+        self.present_value = present_value
+        self.need_value = need_value
+
+    def __str__(self):
+        return f"{self.present_value} fuel is not enough, {self.need_value} is needed."
 
