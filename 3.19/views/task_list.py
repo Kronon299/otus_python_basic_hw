f20657508d7dab6d015d09e03e770f49db7795d2
Stas_Klimovich <stasklimovich299@gmail.com>
2020-10-15 23:34:49 +0300

    task details view was added #5

--- 3.19/views/task_list.py
+++ 3.19/views/task_list.py
@@ -1,8 +1,25 @@
 from flask import Blueprint, render_template
+from werkzeug.exceptions import BadRequest
 
 task_list_app = Blueprint("product_app", __name__)
 
 
+TASKS = {
+    1: 'Помыть посуду',
+    2: 'Вынести мусор',
+    3: 'Сходить в магазин',
+}
+
+
 @task_list_app.route("/")
 def task_list():
-    return render_template("task_list/index.html")
+    return render_template("task_list/index.html", tasks=TASKS)
+
+
+@task_list_app.route("/<int:task_id>/")
+def task_detail(task_id: int):
+    try:
+        task_name = TASKS[task_id]
+    except KeyError:
+        raise BadRequest(f'Invalid task id #{task_id}!')
+    return task_name
