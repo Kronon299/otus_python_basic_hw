abf6a2c1fc5c8c9de8ce2375460078a88ba98047
Stas_Klimovich <stasklimovich299@gmail.com>
2020-10-22 23:12:25 +0300

    "task-done" button was added #5

--- 3.19/views/task_list.py
+++ 3.19/views/task_list.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 from werkzeug.exceptions import BadRequest
 
 task_list_app = Blueprint("task_list_app", __name__)
@@ -16,12 +16,17 @@ def task_list():
     return render_template("task_list/index.html", tasks=TASKS)
 
 
-@task_list_app.route("/<int:task_id>/")
+@task_list_app.route("/<int:task_id>/", methods=['GET', 'DELETE'])
 def task_detail(task_id: int):
     try:
         task_name = TASKS[task_id]
     except KeyError:
         raise BadRequest(f'Invalid task id #{task_id}!')
+
+    if request.method == 'DELETE':
+        TASKS.pop(task_id)
+        return jsonify(ok=True)
+
     return render_template(
         "task_list/detail.html",
         task_id=task_id,
