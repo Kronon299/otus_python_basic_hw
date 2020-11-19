14f02fcd1524c6ae2ffec23091d5c101e4e23fcb
Stas_Klimovich <stasklimovich299@gmail.com>
2020-11-20 01:09:30 +0300

    database was connected with application #7

--- 3.19/task-list/app.py
+++ 3.19/task-list/app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
-
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 from views import task_list_app
+import config
 
 app = Flask(__name__)
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=config.SQLALCHEMY_DATABASE_URI,
+)
 app.register_blueprint(task_list_app, url_prefix="/task_list")
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route("/", methods=["GET", "POST"])
--- 3.19/task-list/config.py
+++ 3.19/task-list/config.py
@@ -1,1 +1,1 @@
-SQLALCHEMY_DATABASE_URI=""
+SQLALCHEMY_DATABASE_URI = "psycopg2://user:user@0.0.0.0:5442/task-list"
