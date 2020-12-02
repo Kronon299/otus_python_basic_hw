d8a3b056a1dddf4131a0195a049718fbf65196c9
Stas_Klimovich <stasklimovich299@gmail.com>
2020-12-03 01:12:01 +0300

    Task model connected to views and templates #7

--- 3.19/task-list/views/task_list.py
+++ 3.19/task-list/views/task_list.py
@@ -1,34 +1,26 @@
 from flask import Blueprint, render_template, request, jsonify
 from werkzeug.exceptions import BadRequest
 
+from models import Task, db
+
 task_list_app = Blueprint("task_list_app", __name__)
-
-
-TASKS = {
-    1: 'Помыть посуду',
-    2: 'Вынести мусор',
-    3: 'Сходить в магазин'
-}
 
 
 @task_list_app.route("/")
 def task_list():
-    return render_template("task_list/index.html", tasks=TASKS)
+    tasks = Task.query.filter_by(deleted=False).all()
+    return render_template("task_list/index.html", tasks=tasks)
 
 
 @task_list_app.route("/<int:task_id>/", methods=['GET', 'DELETE'])
 def task_detail(task_id: int):
-    try:
-        task_name = TASKS[task_id]
-    except KeyError:
+    task = Task.query.filter_by(id=task_id).one_or_none()
+    if task is None:
         raise BadRequest(f'Invalid task id #{task_id}!')
 
     if request.method == 'DELETE':
-        TASKS.pop(task_id)
+        task.deleted = True
+        db.session.commit()
         return jsonify(ok=True)
 
-    return render_template(
-        "task_list/detail.html",
-        task_id=task_id,
-        task_name=task_name,
-    )
+    return render_template("task_list/detail.html", task=task)
