26504bbb1f395f51533a468cd7cbb7e9fb348bb7
Stas_Klimovich <stasklimovich299@gmail.com>
2020-12-03 15:55:19 +0300

    task list was ordered #7

--- 3.19/task-list/views/task_list.py
+++ 3.19/task-list/views/task_list.py
@@ -8,7 +8,7 @@ task_list_app = Blueprint("task_list_app", __name__)
 
 @task_list_app.route("/")
 def task_list():
-    tasks = Task.query.all()
+    tasks = Task.query.order_by(Task.id).all()
     return render_template("task_list/index.html", tasks=tasks)
 
 
